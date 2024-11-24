220305573f49acdd63fa101027c3495dbbdb4e57
Fedor Hajdu <fedorhajdu@gmail.com>
2024-11-24 01:47:00 +0000

    Add Lazy plugin manager

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,9 @@
 --require("fedorh.plugins-setup")
 --require("fedorh.core.colorscheme")
+
 require("fedorh.core")  -- No need for separate files, the core has it's own init
+require("fedorh.lazy")  -- Lazy Plugin manager init here. After this :Lazy command should open the plugin manager
+
 --require("fedorh.core.keymaps")
 --require("fedorh.plugins.comment")
 --require("fedorh.plugins.nvim-tree")
