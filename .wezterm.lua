6cd1c503d719218b2153df6641aee02617a91dc4
Fedor Hajdu <fedorhajdu@gmail.com>
2024-12-04 22:11:06 +0000

    remove commented out lines

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,7 +1,6 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
--- config.color_scheme = 'Nightfly (Gogh)'
 config.tab_bar_at_bottom = true
 config.color_scheme = "Tokyo Night"
 config.use_fancy_tab_bar = false
@@ -11,7 +10,6 @@ config.font_size = 13
 config.font = wezterm.font("InconsolataGo Nerd Font Mono")
 config.front_end = "WebGpu"
 
--- try to start maximized
 config.initial_cols = 300
 config.initial_rows = 70
 
